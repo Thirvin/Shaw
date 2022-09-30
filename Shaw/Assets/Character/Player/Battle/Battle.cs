ad50e6f6d8b6860b1aa0923f8cc7df0f2ba568f2
Thirvin <leesin5202@gmail.com>
2022-09-30 21:48:49 +0800

    Add battle ,contains Attack and Hur.

---
+++ Shaw/Assets/Character/Player/Battle/Battle.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle : MonoBehaviour
+{
+    private void Attack()
+    {
+        //do thing with playermanager
+    }
+    private void Hurt()
+    {
+        //do thing with playmanager
+    }
+}
